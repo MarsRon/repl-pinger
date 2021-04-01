36cfe51d67dfd6df7daa50e40051491e19150a4f
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-02 00:03:22 +0800

    Webhook Alert

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const sites = require("./sites.json");
 const fetch = require("node-fetch");
 const server = require("express")();
+const { WEBHOOK, USER } = process.env;
 server.all("/", (req, res) => {
 	sites.forEach(site => 
 		fetch(site)
-			.then(res => console.log(`${site}: ${res.status}`))
+			.then(res => {
+				if (!res.ok)
+					fetch(WEBHOOK, {
+						method: "post",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							content: `<@${USER}>`,
+							embeds: [{
+								title: "SITE IS DOWN",
+								description: `\`${new URL(site).hostname}\` is down: ${res.status}`,
+								color: 16013612
+							}]
+						}),
+					});
+				console.log(`${site}: ${res.status}`);
+			})
 			.catch(e => console.log(e.message))
 	);
 	res.redirect(307, "https://marsron.github.io");
