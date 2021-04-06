c0bcb764c5dcecb56020a37b93b51bb68d5bcccb
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-07 00:30:06 +0800

    Use HEAD instead of GET

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const sites = require("./sites.json");
-const fetch = require("node-fetch");
-const server = require("express")();
-const { WEBHOOK, USER } = process.env;
-server.all("/", (req, res) => {
-	sites.forEach(site => 
-		fetch(site)
-			.then(res => {
-				const domain = new URL(site).hostname;
-				if (!res.ok)
-					fetch(WEBHOOK, {
-						method: "post",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({
-							content: `<@${USER}>`,
-							embeds: [{
-								title: "SITE IS DOWN",
-								description: `\`${domain}\` is down: ${res.status}`,
-								color: 16013612
-							}]
-						}),
-					});
-				console.log(`${domain}: ${res.status}`);
-			})
-			.catch(e => console.log(e.message))
-	);
-	res.redirect(307, "https://marsron.github.io");
+const axios = require("axios").default;
+const app = require("express")();
+
+app.all("/", (req, res) => {
+	for (const site of process.env.SITES.split(" "))
+		axios.head(site).then(res => {
+			const domain = new URL(site).hostname;
+			if (res < 200 || res >= 300)
+				axios.post(process.env.WEBHOOK, {
+					content: `<@${process.env.USERID}>`,
+					embeds: [{
+						title: "SITE IS DOWN",
+						description: `\`${domain}\` is down: ${res.status}`,
+						color: 16013612
+					}]
+				});
+			console.log(`${domain}: ${res.status}`);
+		}).catch(e => console.log(e.message));
+	res.send("ok");
 });
-server.listen(3000);
+
+app.listen(3000);
