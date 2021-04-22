497d788a6c5124ad6cf21ea946dc53ccfe4a3c92
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-22 16:24:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 require("dotenv").config();
-const axios = require("axios").default;
+const { head, post } = require("axios").default;
 const sites = process.env.SITES.split(" ");
 
 require("http").createServer((req, res) => {
+	res.end("okay");
 	for (const site of sites)
-		axios.head(site, { timeout: 5000 })
+		head(site, { timeout: 5000 })
 			.then(res => console.log(`${new URL(site).hostname}: ${res.statusText}`))
-			.catch(() =>
-				axios.post(process.env.DISCORD_WEBHOOK, {
-					content: `<@${process.env.OWNERID}>`,
-					embeds: [{
-						title: "SITE IS DOWN",
-						description: `${site} is down`,
-						color: 16013612
-					}]
-				})
-			);
-	res.write("ok");
-	res.end();
+			.catch(() => post(process.env.DISCORD_WEBHOOK, {
+				content: `<@${process.env.OWNERID}>`,
+				embeds: [{
+					title: "SITE IS DOWN",
+					description: `${site} is down`,
+					color: 16013612
+				}]
+			}));
 }).listen(3000);
 
 console.log("Repl Pinger is now running!");
