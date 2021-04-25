b38b1a6db6ba3e16949b5958e45a6b61f575f728
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-04-25 20:37:20 +0800

    Lengthen timeout

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const sites = process.env.SITES.split(" ");
 require("http").createServer((req, res) => {
 	res.end("okay");
 	for (const site of sites)
-		head(site, { timeout: 5000 })
+		head(site, { timeout: 10000 })
 			.then(res => console.log(`${new URL(site).hostname}: ${res.statusText}`))
-			.catch(() => post(process.env.DISCORD_WEBHOOK, {
-				content: `<@${process.env.OWNERID}>`,
-				embeds: [{
-					title: "SITE IS DOWN",
-					description: `${site} is down`,
-					color: 16013612
-				}]
-			}));
+			.catch(({ status }) => {
+				console.log(`${site} is down | Status: \`${status}\``);
+				post(process.env.DISCORD_WEBHOOK, {
+					content: `<@${process.env.OWNERID}>`,
+					embeds: [{
+						title: "SITE IS DOWN",
+						description: `${site} is down\nStatus: \`${status}\``,
+						color: 16013612
+					}]
+				});
+			});
 }).listen(3000);
 
 console.log("Repl Pinger is now running!");
