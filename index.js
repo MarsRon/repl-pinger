755b327691400556f924579fbc7320a9d472f14c
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-01 13:09:50 +0800

    Status fix

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-require("dotenv").config();
 const { head, post } = require("axios").default;
-const sites = process.env.SITES.split(" ");
+require("dotenv").config();
+const sites = process.env.SITES.split(/\s+/);
 
-require("http").createServer((req, res) => {
+require("http").createServer(async (req, res) => {
+	res.writeHead(200);
 	res.end("okay");
-	for (const site of sites)
-		head(site, { timeout: 10000 })
-			.then(res => console.log(`${new URL(site).hostname}: ${res.statusText}`))
-			.catch(({ status }) => {
-				console.log(`${site} is down | Status: \`${status}\``);
-				post(process.env.DISCORD_WEBHOOK, {
-					content: `<@${process.env.OWNERID}>`,
-					embeds: [{
-						title: "SITE IS DOWN",
-						description: `${site} is down\nStatus: \`${status}\``,
-						color: 16013612
-					}]
-				});
-			});
+	for (const site of sites) {
+		try {
+			const siteRes = await head(site, { timeout: 10000 });
+			console.log(`${new Date().toISOString()} ${new URL(site).hostname}: ${siteRes.statusText}`);
+		} catch(e) {
+			console.log(`Website Down: ${new URL(site).hostname} - ${e.message}`);
+			post(process.env.DISCORD_WEBHOOK, {
+				content: `<@${process.env.OWNERID}>`,
+				embeds: [{
+					title: "SITE IS DOWN",
+					description: `${site} is down\nStatus: \`${e}\``,
+					color: 16013612
+				}]
+			}).catch(e => console.log("Webhook Error: " + e.message));
+		}
+	}
 }).listen(3000);
 
 console.log("Repl Pinger is now running!");
