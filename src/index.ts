5175c10575b9ac774bc77e43b34e4653cda9e0bc
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-06 21:47:12 +0800

    Typescript-ify lol

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+// Modules
+import axios from "axios";
+import { createServer } from "http";
+import { URL } from "url";
+
+// Load environment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+// Constants
+const { SITES, DISCORD_WEBHOOK, OWNERID } = process.env,
+	sites = (SITES as string).split(/\s+/),
+	print = console.log;
+
+// Create webserver
+createServer(async (_req, svrRes) => {
+	// Respond with "okay"
+	svrRes.writeHead(200);
+	svrRes.end("okay");
+
+	// Loop through sites
+	for (const site of sites) {
+		try {
+			// Try pinging the site
+			const res = await axios.head(site, { timeout: 20000 });
+			print(`${new Date().toISOString()} ${new URL(site).hostname}: ${res.statusText}`);
+		} catch(err) {
+			// Log to console & Send Discord webhook
+			print(`Website Down: ${new URL(site).hostname} - ${err.message}`);
+			axios.post(DISCORD_WEBHOOK as string, {
+				content: `<@${OWNERID as string}>`,
+				embeds: [{
+					title: "SITE IS DOWN",
+					description: `${site} is down\nTimestamp: ${new Date().toISOString()}\nStatus: \`${err}\``,
+					color: 16013612
+				}]
+			}).catch(e => // Catch webhook error
+				print("Webhook Error: " + e.message)
+			);
+		}
+	}
+}).listen(3000, () => // Listen on port 3000
+	print("Repl Pinger is now running!")
+);
