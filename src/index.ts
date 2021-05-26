b57d871d52aaf281ce0a1922f0211c0b26836bb9
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-05-26 22:44:56 +0800

    Improve index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 // Modules
 import axios from "axios";
-import { createServer } from "http";
-import { URL } from "url";
+import http from "http";
 
 // Load environment variables
 import dotenv from "dotenv";
@@ -14,7 +13,7 @@ const { SITES, DISCORD_WEBHOOK, OWNER_ID } = process.env;
 const sites = SITES!.split(/\s+/);
 
 // Create webserver
-createServer((_req, svrRes) => {
+http.createServer((_req, svrRes) => {
 
 	// Respond with "okay"
 	svrRes.writeHead(200);
@@ -22,18 +21,21 @@ createServer((_req, svrRes) => {
 
 	// Loop through sites
 	sites.forEach(async site => {
+
 		try {
+
 			// Try pinging the site
 			const res = await axios.head(site, { timeout: 30000 });
-			console.log(`${new Date().toISOString()} ${new URL(site).hostname}: ${res.statusText}`);
+			console.log(`${new Date().toISOString()} ${site}: ${res.statusText}`);
+
 		} catch({ message: error }) {
 
 			// Log to console
 			console.log(`Website Down: ${site} - ${error}`);
 
 			// Check for status code
-			// Return of status is OK
-			const status = Number((error as string).match(/\d{3}$/));
+			// Return if response isn't 40x or 50x error
+			const status = Number((error as string).slice(-3));
 			if (status < 400) return;
 
 			// Send Discord webhook
@@ -44,15 +46,19 @@ createServer((_req, svrRes) => {
 			}];
 			axios.post(
 				DISCORD_WEBHOOK!,
-				OWNER_ID !== undefined ? { content: `<@${OWNER_ID}>`, embeds } : { embeds }
-			).catch(e => // Catch webhook error
+				{ content: OWNER_ID ? `<@${OWNER_ID}>` : undefined, embeds }
+			).catch(e =>
+				// Catch webhook error
 				console.log("Webhook Error: " + e.message)
 			);
+
 		}
+
 	});
 
 })
+
 // Listen on port 3000
-.listen(3000, () => 
+.listen(3000, () =>
 	console.log("Repl Pinger is now running on port 3000!")
 );
